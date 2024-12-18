56fce94a22375370ef2e8ad377df34f30bbb15a6
djsmk123 <djsmk123@gmail.com>
2024-12-18 19:48:56 +0530

    added url

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
           SmkWinner
         </a>
         <a
-          href="https://github.com/stackblitz/web-ext-starter"
+          href="https://github.com/Djsmk123/YoutubeUnWrapped"
           target="_blank"
           rel="noopener noreferrer"
           className="text-gray-600 hover:text-gray-700"
